12d1445669ceb4547071e70ec24e5a215754aa0a
kshku <1211shree@gmail.com>
2025-12-01 19:54:41 +0530

    modify to take parameters

--- test.c
+++ test.c
@@ -2,9 +2,16 @@
 
 #include "src/regex.h"
 
-int main(void) {
-    const char *text = "somebody saw nobody";
-    const char *re = "saw";
+int main(int argc, const char **argv) {
+    if (argc != 3) {
+        printf("Usage: regexer \"<text>\" \"<regex>\"\n");
+        return -1;
+    }
+
+    const char *text = argv[1];
+    const char *re = argv[2];
+    // const char *text = "somebody saw nobody";
+    // const char *re = "saw";
 
     Regex regex;
     regex_create(&regex, re);
